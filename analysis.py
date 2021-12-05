a594c1bac2eb8ec95614f9a2b11bab1291b2c981
taucmar <antoinelelongpro@gmail.com>
2021-12-05 16:39:21 +0100

    scraper.py can get comments, and stock everything in comments.csv

---
+++ analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = pd.read_csv("./comments.csv")
+
+if __name__=="__main__":
+    print(data)
--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from googleapiclient.discovery import build
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 apikey = os.getenv('APIKEY')
@@ -11,17 +12,35 @@ request = youtube.channels().list(
     part='statistics',
     forUsername='dirtybiology'
 )
+general_statistics = request.execute()
+channel_id = general_statistics['items'][0]['id']
 
-request = youtube.search().list(
-    part="snippet",
-    eventType="completed",
-    q="dirtybiology",
-    type='video',
-    maxResults=3
-)
-response = request.execute()
+# Scraping des commentaires des vidéos cibles
+
+video_ids = ['ehmyaX0lJew']
+comments_list = []
+
+for videoId in video_ids:
+    # Requête pour obtenir jusqu'à 100 (maximum autorisé par l'API) commentaires sur une vidéo YouTube
+    request = youtube.commentThreads().list(
+        part="snippet",
+        videoId='ehmyaX0lJew',
+        maxResults=100,
+        order='relevance'
+    )
+    response = request.execute()
+
+    # Stockage des sorties dans une dataframe
+    for item in response['items']:
+        snippet = item['snippet']['topLevelComment']['snippet']
+        snippet['authorChannelId'] = snippet['authorChannelId']['value']  # lissage
+        comments_list.append(snippet)
+
+data = pd.DataFrame(comments_list)
+
+# Stockage dans un fichier en dehors du script
+data.to_csv(path_or_buf="./comments.csv",
+            index=False)
 
 if __name__ == "__main__":
-    for item in response['items']:
-        print("\n", item['id']
-              , item['snippet'])
+    print(data)
