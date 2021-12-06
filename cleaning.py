78bb12ad5a92fa8aea4ec8ce2c19e73fe93492a7
yseultmasson <yseult.masson@gmail.com>
2021-12-06 17:05:07 +0100

    Vraie fonction de nettoyage

--- cleaning.py
+++ cleaning.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import re
 
 data = pd.read_csv("./comments.csv")
 
-text = data[["textDisplay"]][0]
-text = text.lower()
-text
+#Fonction de nettoyage très light. Si on veut plus nettoyer, utiliser le module re, voir dans liens utiles/NLP
+
+def cleaning(text):
+    text = text.lower()
+    text = text.replace('\n', ' ').replace('\r', '')
+    text = ' '.join(text.split())
+    return text
+
+
+#ajout d'une colonne à data avec le texte nettoyé
+
+data[["textClean"]] = data[["textOriginal"]].apply(cleaning)
