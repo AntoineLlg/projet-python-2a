d8d34d4c55c6f39ca37dd40cb48c06cd75aa509a
taucmar <antoinelelongpro@gmail.com>
2021-12-06 17:14:39 +0100

    cleaned some code

--- cleaning.py
+++ cleaning.py
@@ -3,7 +3,7 @@ import re
 
 data = pd.read_csv("./comments.csv")
 
-#Fonction de nettoyage très light. Si on veut plus nettoyer, utiliser le module re, voir dans liens utiles/NLP
+# Fonction de nettoyage très light. Si on veut plus nettoyer, utiliser le module re, voir dans liens utiles/NLP
 
 def cleaning(text):
     text = text.lower()
@@ -12,6 +12,6 @@ def cleaning(text):
     return text
 
 
-#ajout d'une colonne à data avec le texte nettoyé
+# ajout d'une colonne à data avec le texte nettoyé
 
 data[["textClean"]] = data[["textOriginal"]].apply(cleaning)
