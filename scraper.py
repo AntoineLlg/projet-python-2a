f699507c16740612bef711897cde6bb78446ded4
Antoine Lelong <79382600+taucmar@users.noreply.github.com>
2021-12-06 15:25:04 +0100

    scraper.py can fetch video id of up to 20 videos (#2)
    Co-authored-by: taucmar <antoinelelongpro@gmail.com>

--- scraper.py
+++ scraper.py
@@ -15,12 +15,24 @@ request = youtube.channels().list(
 general_statistics = request.execute()
 channel_id = general_statistics['items'][0]['id']
 
+request = youtube.search().list(
+    part="id,snippet",
+    channelId=channel_id,
+    order='date',
+    maxResults=20
+)
+response = request.execute()
+video_ids = []
+more_info = []
+for item in response['items']:
+    video_ids.append(item['id']['videoId'])
+    more_info.append({'videoTitle': item['snippet']['title'], 'videoDate': item['snippet']['publishedAt']})
 # Scraping des commentaires des vidéos cibles
 
-video_ids = ['ehmyaX0lJew']
 comments_list = []
 
 for videoId in video_ids:
+    specific_info = more_info[video_ids.index(videoId)]
     # Requête pour obtenir jusqu'à 100 (maximum autorisé par l'API) commentaires sur une vidéo YouTube
     request = youtube.commentThreads().list(
         part="snippet",
@@ -34,7 +46,7 @@ for videoId in video_ids:
     for item in response['items']:
         snippet = item['snippet']['topLevelComment']['snippet']
         snippet['authorChannelId'] = snippet['authorChannelId']['value']  # lissage
-        comments_list.append(snippet)
+        comments_list.append({**snippet, **specific_info})
 
 data = pd.DataFrame(comments_list)
 
